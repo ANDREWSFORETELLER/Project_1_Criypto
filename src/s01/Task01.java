4e0836c26131b6c02301b4efa47e4f6541bec139
Foreteller <Papinat@bk.ru>
2022-01-19 23:14:27 +0300

    add clone

--- src/s01/Task01.java
+++ src/s01/Task01.java
@@ -3,7 +3,7 @@ package s01;
 public class Task01 {
     public static void main(String[] args) {
         System.out.println("Hayid");
-        System.out.println("sdsdsdf sdfsdf");
+        System.out.println("sdsdsdf eeeee");
 
     }
 }
