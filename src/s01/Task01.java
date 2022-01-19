56ec6e608dde6e9fe63f3f5421eb8ff6e2a4db25
Foreteller <Papinat@bk.ru>
2022-01-19 19:58:10 +0300

    Test One

--- src/s01/Task01.java
+++ src/s01/Task01.java
@@ -4,7 +4,6 @@ public class Task01 {
     public static void main(String[] args) {
         System.out.println("Hayid");
         System.out.println("sdsd");
-        System.out.println("sdsd");
-        System.out.println("sdsd");
+
     }
 }
